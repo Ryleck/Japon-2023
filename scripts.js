012d9cd584246021a2c3dfce4ebd704f414c92e1
Ryleck <ryleck@gmail.com>
2023-05-03 12:43:40 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
 /**
  * Created by phili on 2023-05-01.
  */
-import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
-
-const animateTitle = document.querySelector("#divTitle");
-
-const animatedTitleTimeline = new ScrollTimeline({
-    
-    scrollOffsets: [CSS.percent(0), CSS.percent(100)]
-});
-
-animateTitle.animate({
-    transform: ['height(1)','height(0)']
-});
